6707961b9cde993b88f0bc4005d914355c777fb6
enrique343 <enriqueg0604@gmail.com>
2024-10-02 18:49:40 -0500

    made the code seperated into two seperate functions

--- app.py
+++ app.py
@@ -5,20 +5,35 @@ import os
 api_key       = os.getenv('KEY_NAME')
 api_secret     = os.getenv('PRIVATE_KEY')
 
+global jwt
+jwt=""
 
-request_method = "GET"
-request_path = "/api/v3/brokerage/accounts"
+def make_token():
+    global jwt
+    request_method = "GET"
+    request_path = "/api/v3/brokerage/accounts"
 
-jwt_uri = jwt_generator.format_jwt_uri(request_method, request_path)
-jwt_token = jwt_generator.build_rest_jwt(jwt_uri, api_key, api_secret)
+    jwt_uri = jwt_generator.format_jwt_uri(request_method, request_path)
+    jwt_token = jwt_generator.build_rest_jwt(jwt_uri, api_key, api_secret)
+
+    jwt= jwt_token
 
 
-url= 'https://api.coinbase.com/api/v3/brokerage/accounts'        #gets the cards requested
-headers = {
-    "Authorization":f'Bearer {jwt_token}',
-    "Content-Type": "application/json" ,
-}
-r = pip._vendor.requests.get(url, headers=headers)
-print(r)
-res=r.json()
-print(res.keys())
+
+def test_token():
+    global jwt
+
+    url= 'https://api.coinbase.com/api/v3/brokerage/accounts'        #gets the cards requested
+    headers = {
+        "Authorization":f'Bearer {jwt}',
+        "Content-Type": "application/json" ,
+    }
+    r = pip._vendor.requests.get(url, headers=headers)
+    print(r)
+    
+def main():
+    make_token()
+    test_token()
+    
+if __name__ == "__main__":
+    main()
